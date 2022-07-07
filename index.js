7539d993a6a0eecf2b1b0e3800a49c4578e46b92
Saul Ventura <VentSaulCS@outlook.com>
2022-07-06 20:50:28 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// const inquirer = require('inquirer')
+const inquirer = require('inquirer')
+// import inquirer from 'inquirer';
 const mysql = require('mysql2');
 require('console.table')
 require("dotenv").config()
@@ -17,5 +18,61 @@ const db = mysql.createConnection(
   db.connect(function(err){
     if(err) throw err;
     console.log("Welcome to SAUL's Employee Tracker Maangement System")
-
+   startApp()
   })
+
+  function startApp(){
+    inquirer.prompt([
+        {
+            type:"list",
+            message:"What would you like to do?",
+            choices:["View Departments","View Employees", "View Roles", "Add Department", "Add Employess", "Exit App"],
+            name:"option"
+        }
+    ]).then(function(response){
+        console.log(response);
+        switch (response.option){
+            case "View Departments":
+                viewdepartments()
+                break;
+            case "View Employees":
+                viewemployees()    
+                break;
+            case "View Roles":
+                viewroles()
+                break;
+            case "Add Department":
+                adddepartment()
+                break        
+            case "Add Employees":
+                addemployees()
+                break;
+          default:
+            db.end();
+            process.exit(0)   ;
+        }
+    })
+  }
+
+
+  function viewdepartments(){
+    db.query("select * from department;",function(err,data){
+        if (err) throw err;
+        console.table(data);
+        startApp();
+    })
+  }
+  function viewroles(){
+    db.query("select * from roles;",function(err,data){
+        if (err) throw err;
+        console.table(data);
+        startApp();
+    })
+  }
+  function viewemployees(){
+    db.query("select * from Employees;",function(err,data){
+        if (err) throw err;
+        console.table(data);
+        startApp();
+    })
+  }
